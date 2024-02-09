9087f70b28e5ed50e71dd682143ca6b84b8120cd
Sooraj Singh <soorajsingh7505@gmail.com>
2024-02-09 17:13:24 +0530

    upload

--- client/src/Components/Account/Register.jsx
+++ client/src/Components/Account/Register.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./mix.css";
 
 const Register = () => {
+  const [sendData, setSendData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cpassword: "",
+    role: ""
+  });
+
+  const changeData = (e) => {
+    setSendData({
+      ...sendData,
+      [e.target.name]: e.target.value
+    });
+  };
+  console.log(sendData);
+
   return (
     <div className="reg">
       <div className="regContainer">
@@ -13,7 +31,27 @@ const Register = () => {
           <input type="text" placeholder="Enter your name" />
         </div>
         <div className="form">
-            
+          <label htmlFor="email">Email</label>
+          <br />
+          <input type="email" placeholder="Enter your email" />
+        </div>
+        <div className="form">
+          <label htmlFor="password">Password</label>
+          <br />
+          <input type="password" placeholder="Enter password" />
+        </div>
+        <div className="form">
+          <label htmlFor="cpassword">Confirm Password</label>
+          <br />
+          <input type="password" placeholder="Enter confirm password" />
+        </div>
+        <div className="form">
+          <button>Register</button>
+        </div>
+        <div className="form">
+          <p>
+            Already have an Account? <NavLink to="/">Login</NavLink>
+          </p>
         </div>
       </div>
     </div>
