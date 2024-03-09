5b8b8565be8101c2ee6896d15d1b3ff3937df01d
Sooraj Singh <soorajsingh7505@gmail.com>
2024-03-09 11:26:41 +0530

    upload

--- client/src/Components/Config.js
+++ client/src/Components/Config.js
@@ -1,5 +1,9 @@
 const config = {
-  url: "http://localhost:4000"
+  url: "https://food-delivery-app-3-0-server.vercel.app"
 };
 
 export default config;
+
+// https://food-delivery-app-3-0-server.vercel.app/
+
+// http://localhost:4000
